53301259a5768e1a39fe70b5c26e42cf2c53fc3f
Fachri Naufal <fachrinfl@Fachris-MacBook-Pro.local>
2022-12-26 08:58:18 +0700

    init project

---
+++ src/components/Title/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+import styles from './styles';
+
+const Title = () => {
+    return (
+        <Text style={styles.tictle}>Hello</Text>
+    );
+};
+
+export default Title;
---
+++ src/screens/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import Title from '../../components/Title';
+
+const Home = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Title />
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default Home;
